ba1c457ef06c36547471529cf8de7ce7139e2b54
Taha Eren Sessevmez <tahaeren1997@gmail.com>
2021-04-20 22:38:26 +0300

    PostList Home.vue çekildi

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,27 +1,41 @@
 <template>
-    <div>
+  <div>
+    <b-container fluid>
+      <b-row>
+        <b-col>
           <b-button variant="danger">Button</b-button>
-
-      <router-link to="/register">Register</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/post">Post</router-link>
+          <router-link to="/register">Register</router-link> |
+          <router-link to="/login">Login</router-link> |
+          <router-link to="/post">Post</router-link>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="3">Taha</b-col>
+        <b-col cols="6">
+            <post-list></post-list>
+        </b-col>
+        <b-col cols="3">sessevmez</b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "Home Tet",
-        data() {
-            return {
-                selectedItem: null
-            }
-        },
-        methods: {
-            
-        }
-    }
+import PostList from "../components/PostList.vue";
+
+export default {
+     components: {
+    "post-list": PostList,
+  },
+  name: "Home Tet",
+  data() {
+    return {
+      selectedItem: null,
+    };
+  },
+  methods: {},
+};
 </script>
 
 <style>
-    
 </style>
