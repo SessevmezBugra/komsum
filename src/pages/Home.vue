8d5b34c7d4784453afb34f02d58c4f394a5f023b
Taha Eren Sessevmez <tahaeren1997@gmail.com>
2021-04-21 04:42:39 +0300

    Homepage ve liste getirildi bubble componenti oluşturuldu

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="app"  v-bind:style="{ backgroundColor: color}">
     <b-container fluid>
       <b-row>
         <b-col>
@@ -10,13 +10,22 @@
         </b-col>
       </b-row>
       <b-row>
-        <b-col cols="3">Taha</b-col>
+        <b-col cols="3">SOL</b-col>
         <b-col cols="6">
             <post-list></post-list>
         </b-col>
-        <b-col cols="3">sessevmez</b-col>
+        <b-col cols="3">SAĞ</b-col>
       </b-row>
     </b-container>
+    <div>
+      <div >
+<h1>Arkaplan rengi</h1>
+  <div class="color">
+<input type="text" v-bind:style="{ color: color}" v-model="color" maxlength="7" placeholder="#000000"/>
+  <input type="color" v-model="color"/>
+    </div>
+</div>
+    </div>
   </div>
 </template>
 
@@ -31,6 +40,7 @@ export default {
   data() {
     return {
       selectedItem: null,
+      color: {color: ''}
     };
   },
   methods: {},
@@ -38,4 +48,31 @@ export default {
 </script>
 
 <style>
+#app {
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  input {
+    padding: 1rem 1rem;
+    font-size: 2rem;
+    border: 0;
+    border-radius: 3px;
+    background: #fff;
+  }
+  
+}
+
+
+
+input[type="color"]{
+  width:40px;
+  border:1px solid black;
+  margin:0px;
+  padding:0px !important;
+  height:40px;
+}
+
+
 </style>
