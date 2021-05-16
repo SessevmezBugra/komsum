d2fddf9d67008b269efe9b553a24297dcf19e0b7
Hilal <hilallyeniyurtt@gmail.com>
2021-05-16 20:19:23 +0300

    Post list and menu list developed.

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <b-container >
-      <b-row align-v="center"  id="post-component" >
-        <b-col class="definition" :avatar="avatar"><b-avatar src="https://placekitten.com/300/300"></b-avatar>
-      {{name}}</b-col>   
-       <b-col class="date">{{date}}</b-col>
+    <b-container>
+      <b-row align-v="center" id="post-component">
+        <b-col class="definition" :avatar="avatar"
+          ><b-avatar src="https://placekitten.com/300/300"></b-avatar>
+          {{ userName }}
+
+          <p class="content">{{ content }}</p>
+        </b-col>
+        <b-col class="date">{{ date }}</b-col>
       </b-row>
     </b-container>
   </div>
@@ -12,36 +16,38 @@
 
 
 <script>
-
-export default{
-
-    props: ["avatar", "name", "date"]
+export default {
+  props: ["avatar", "userName", "content", "date"],
 };
 </script>
 
 <style>
-
-@import url('https://fonts.googleapis.com/css2?family=PT+Serif&display=swap');
- #post-component{
+@import url("https://fonts.googleapis.com/css2?family=PT+Serif&display=swap");
+#post-component {
   border: 1px solid;
   box-shadow: 2px 2px 2px 2px #c4c7be;
   border: 1px solid rgb(231, 231, 231);
   padding: 10px;
   border-radius: 15px;
+  cursor: pointer;
 }
-#post-component:hover{
-     background: rgb(250, 250, 250); 
-     box-shadow: 1px 5px 2px rgb(186, 186, 187);
-} 
-.date{
+#post-component:hover {
+  background: rgb(250, 250, 250);
+  box-shadow: 1px 5px 2px rgb(186, 186, 187);
+}
+.date {
   text-align: right;
-   padding-bottom: 75px;
-  font-family: 'PT Serif', serif;
+  font-family: "PT Serif", serif;
+  margin-bottom: 100px;
 }
-.definition{
-   text-align: left;
-   padding-bottom: 50px;
-   font-family: 'PT Serif', serif;
+.definition {
+  text-align: left;
+  font-family: "PT Serif", serif;
+}
+
+.content {
+  text-align: left;
+  margin-top: 15px;
 }
 </style>
 
--- src/components/menu/MenuItem.vue
+++ src/components/menu/MenuItem.vue
@@ -1,38 +1,44 @@
 <template>
-    
-<b-container class="menu-container">
-  <b-row >
-    <b-col cols="2">  <b-icon :icon="icon" font-scale="1"></b-icon></b-col>
-    <b-col cols="10"> {{text}} </b-col>
-  </b-row>
-</b-container>
-
+  <b-container class="menu-container">
+    <b-row>
+      <!-- <b-col cols="2"> <b-icon :icon="icon" font-scale="1"></b-icon></b-col>
+      <p class="h1 mb-2">Icon <b-icon icon="exclamation-circle-fill"></b-icon></p> -->
+      <b-col cols="12"  > <b-icon :icon="icon" font-scale="1"  ></b-icon> {{ text }}  </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default{
-    props: ["icon", "text"]
-}
+export default {
+  props: ["icon", "text"],
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap");
 
-@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap');
+.menu-container {
+  background-color: white;
+  border: 2px solid white;
+  border-radius: 25px;
+  transition: box-shadow 0.1;
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 23px;
+  width: 190px !important;
+  margin-top: 25px;
+  cursor: pointer;
+  
 
-.menu-container{
-    background-color:white;
-    border: 2px solid white;
-    border-radius: 25px;
-     transition: box-shadow .1;
-     font-family:'Josefin Sans', sans-serif;
-     font-size: 23px;
-     width: 190px !important;
-     margin-top: 25px;
-
+      
 }
-.menu-container:hover{
-     box-shadow: 0px 5px 5px skyblue;
-     transition: box-shadow .3;
-    
-} 
+.menu-container:hover {
+  box-shadow: 0px 5px 5px skyblue;
+  transition: box-shadow 0.3;
+}
+.b-icon{
+  float:left;
+  margin-right: 15px;
+  margin-top: 3px;
+}
+
 </style>
