3733d8f78358980caea7537e55042ef8b81bac0f
Taha Eren Sessevmez <tahaeren1997@gmail.com>
2021-06-15 15:07:50 +0300

    Alt menu secenekleri tiklaninca renklendirildi

--- src/components/menu/MobileMenuList.vue
+++ src/components/menu/MobileMenuList.vue
@@ -1,78 +1,50 @@
 <template>
-  <!-- <nav class="nav">
-        <a href="#" class="nav__link">
-           
-        </a>
-        <a href="#" class="nav__link nav__link--active">
-          
-        </a>
-        <a href="#" class="nav__link">
-             
-        </a>
-        <a href="#" class="nav__link">
-           
-        </a>
-         <a href="#" class="nav__link">
-             <b-icon icon="person-bounding-box" variant="danger"></b-icon>
-        </a>
-        <a href="#" class="nav__link">
-        
-        </a>
-    </nav> -->
   <b-nav tabs justified class="position-fixed fixed-bottom bg-light">
-    <b-nav-item class="m-0">
-      <b-row class="text-center">
-        <b-col>
-           <b-container class="justify-content-center">
+    <b-nav-item class="m-0" @click="iconSelected('A')">
+      <b-row>
+        <b-col class="d-flex justify-content-center">
           <b-icon
+            :class="{ active: houseDoor }"
             icon="house-door-fill"
-            class="m-0 p-0"
+            class="m-0 p-0 icon"
             font-scale="0.9"
-            variant="success"
           ></b-icon>
-           </b-container>
         </b-col>
       </b-row>
     </b-nav-item>
-    <b-nav-item class="m-0">
-      <b-row class="text-center">
-        <b-col>
-           <b-container class="justify-content-center">
+    <b-nav-item class="m-0" @click="iconSelected('B')">
+      <b-row>
+        <b-col class="d-flex justify-content-center">
           <b-icon
+            :class="{ active: sunGlasses }"
             icon="sunglasses"
-            class="m-0 p-0"
+            class="m-0 p-0 icon"
             font-scale="0.9"
-            variant="primary"
           ></b-icon>
-           </b-container>
         </b-col>
       </b-row>
     </b-nav-item>
-    <b-nav-item class="m-0">
-      <b-row class="text-center">
-        <b-col>
-          <b-container class="justify-content-center">
-            <b-icon
-              icon="bell-fill"
-              class="m-0 p-0"
-              font-scale="0.9"
-              variant="secondary"
-            ></b-icon>
-          </b-container>
+    <b-nav-item class="m-0" @click="iconSelected('C')">
+      <b-row>
+        <b-col class="d-flex justify-content-center">
+          <b-icon
+            :class="{ active: bellIcon }"
+            icon="bell-fill"
+            class="m-0 p-0 icon"
+            font-scale="0.9"
+          ></b-icon>
         </b-col>
       </b-row>
     </b-nav-item>
-    <b-nav-item class="m-0">
-      <b-row class="text-center">
-        <b-col>
-          <b-container class="justify-content-center">
-            <b-icon
-              icon="envelope-fill"
-              class="m-0 p-0"
-              font-scale="0.9"
-              variant="info"
-            ></b-icon>
-          </b-container>
+    <b-nav-item class="m-0" @click="iconSelected('D')">
+      <b-row>
+        <b-col class="d-flex justify-content-center">
+          <b-icon
+            :class="{ active: envelopeIcon }"
+            icon="envelope-fill"
+            class="m-0 p-0 icon"
+            font-scale="0.9"
+          ></b-icon>
         </b-col>
       </b-row>
     </b-nav-item>
@@ -88,29 +60,71 @@
         </b-col>
       </b-row>
     </b-nav-item> -->
-    <!-- <b-nav-item class="m-0">
+    <b-nav-item class="m-0" @click="iconSelected('E')">
       <b-row>
         <b-col>
           <b-icon
+            :class="{ active: circleIcon }"
             icon="plus-circle-fill"
             class="m-0 p-0"
             font-scale="0.9"
-            variant="info"
           ></b-icon>
         </b-col>
       </b-row>
-    </b-nav-item> -->
+    </b-nav-item>
   </b-nav>
 </template>
 
 <script>
 export default {
-  components: {},
+  data() {
+    return {
+      houseDoor: false,
+      sunGlasses: false,
+      bellIcon: false,
+      envelopeIcon: false,
+      circleIcon: false,
+    };
+  },
+  methods: {
+    iconSelected(icon) {
+      if (icon === "A") {
+        this.houseDoor = !this.houseDoor;
+        (this.sunGlasses = false),
+          (this.bellIcon = false),
+          (this.envelopeIcon = false),
+          (this.circleIcon = false);
+      } else if (icon === "B") {
+        this.sunGlasses = !this.sunGlasses;
+        (this.houseDoor = false),
+          (this.bellIcon = false),
+          (this.envelopeIcon = false),
+          (this.circleIcon = false);
+      } else if (icon === "C") {
+        this.bellIcon = !this.bellIcon;
+        (this.houseDoor = false),
+          (this.sunGlasses = false),
+          (this.envelopeIcon = false),
+          (this.circleIcon = false);
+      } else if (icon === "D") {
+        this.envelopeIcon = !this.envelopeIcon;
+        (this.houseDoor = false),
+          (this.sunGlasses = false),
+          (this.bellIcon = false),
+          (this.circleIcon = false);
+      } else if (icon === "E") {
+        this.circleIcon = !this.circleIcon;
+        (this.houseDoor = false),
+          (this.sunGlasses = false),
+          (this.bellIcon = false),
+          (this.envelopeIcon = false);
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
-
 /*
 .nav__link {
   color: #444444;
@@ -130,4 +144,14 @@ export default {
 .nav__icon {
   font-size: 18px;
 } */
+/* @media (max-width: 540px) {
+        .nav-link  {
+          padding: 0px 0px !important;
+        }
+   }    */
+
+.active {
+  color: rgb(65, 66, 65);
+  
+}
 </style>
