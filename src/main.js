226bc1011188aa48cacb217479811511fcc24ba5
Bugra SESSEVMZ <sessevmezbugra@gmail.com>
2021-07-29 21:25:27 +0300

    test

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import store from "./store";
+import Keycloak from "keycloak-js";
 
 
 Vue.use(BootstrapVue)
@@ -36,8 +37,41 @@ const router = new VueRouter({
     routes // short for `routes: routes`
 });
 
-new Vue({
-    router,
-    store,
-    render: h => h(App)
-}).$mount("#app");
+let initOptions = {
+    url: process.env.VUE_APP_KEYCLOAK_URI + '/auth', realm: 'komsumdannehaber', clientId: 'ui-app', onLoad: 'login-required'
+}
+
+let keycloak = Keycloak(initOptions);
+
+keycloak.init({ onLoad: initOptions.onLoad }).then((auth) => {
+    if (!auth) {
+        window.location.reload();
+    } else {
+        console.log("Authenticated");
+
+        new Vue({
+            el: '#app',
+            router,
+            store,
+            render: h => h(App, { props: { keycloak: keycloak } })
+        }).$mount("#app");
+    }
+
+
+    //Token Refresh
+    setInterval(() => {
+        keycloak.updateToken(70).then((refreshed) => {
+            if (refreshed) {
+                console.log('Token refreshed' + refreshed);
+            } else {
+                console.log('Token not refreshed, valid for '
+                    + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+            }
+        }).catch(() => {
+            console.log('Failed to refresh token');
+        });
+    }, 6000)
+
+}).catch(() => {
+    console.log("Authenticated Failed");
+});
